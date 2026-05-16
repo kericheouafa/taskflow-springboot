91ef1bf1b9ecb82ee52f2d89164faf0535e2f08e
kericheouafa <ouafakeriche@gmail.com>
2026-05-16 16:46:03 +0200

    J11 - Ajout Swagger/OpenAPI

--- src/main/java/com/taskflow/taskflow/TaskflowApplication.java
+++ src/main/java/com/taskflow/taskflow/TaskflowApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class TaskflowApplication {
 
-	public static void main(String[] args) {System.out.println("hello");
+	public static void main(String[] args) {
 		SpringApplication.run(TaskflowApplication.class, args);
 	}
 
